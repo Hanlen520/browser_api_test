cd1285c887efc853481cec9d8d3b5b1e61e36731
sxw <sxwollo@hotmail.com>
2017-12-11 02:08:14 +0800

    更新md5计算方法 优化下载

--- test_tools/package_md5_validator.py
+++ test_tools/package_md5_validator.py
@@ -4,51 +4,40 @@
 import unittest
 import requests
 import hashlib
-import os
-
+import os, sys
+import datetime
 
 def md5sum(fname):
-    def read_chunks(fh):
-        fh.seek(0)
-        chunk = fh.read(8096)
-        while chunk:
-            yield chunk
-            chunk = fh.read(8096)
-        else:
-            fh.seek(0)
-
-    m = hashlib.md5()
-    if isinstance(fname, str) and os.path.exists(fname):
-        with open(fname, 'rb') as fh:
-            for chunk in read_chunks(fh):
-                m.update(chunk)
-    elif fname.__class__.__name__ in ['StringIO', "StringO"] or isinstance(fname, file):
-        for chunk in read_chunks(fname):
-            m.update(chunk)
-    else:
-        return ""
-    return m.hexdigest()
+    with open(fname, 'rb') as f:
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        hash = md5obj.hexdigest()
+        return hash
 
 
-def download_apk(apk_url):
-    resp = requests.get(apk_url, stream=True)
-    apk_fname = resp.url.split('/', -1)[-1]
-    f = open(apk_fname, 'wb')
-    for chunk in resp.iter_content(chunk_size=512):
-        if chunk:
-            f.write(chunk)
-    return apk_fname
+def download_apk(url):
+    r = requests.get(url, stream=True)
+    fname = r.url.split('/')[-1]
+    with open(fname, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    return fname
 
 
 def compare_md5():
     config_url = ''
     j = requests.get(config_url).json()
     for pkg in j['rtpkgs']['list']:
+        start_time = datetime.datetime.now().replace(microsecond=0)
         pkg_name = download_apk(pkg['url'])
-        pkg_md5 = md5sum(pkg_name)
-        config_md5 = pkg['md5']
+        end_time = datetime.datetime.now().replace(microsecond=0)
+        print(start_time - end_time)
+        pkg_md5 = md5sum(os.getcwd() + os.path.sep + pkg_name)
+        config_md5 = pkg['md5'].lower()
         if pkg_md5 != config_md5:
-            print(pkg['pkg'])
+            print("Wrong MD5 packages: " + pkg['pkg'])
 
 
 compare_md5()
